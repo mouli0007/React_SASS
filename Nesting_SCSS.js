f9512116815130f35a27acd5d61169d320511239
Mouli VJ <75278840+mouli0007@users.noreply.github.com>
2023-04-29 23:18:58 +0530

    Create Nesting_SCSS.js

---
+++ Nesting_SCSS.js
@@ -0,0 +1,18 @@
+.main-div {
+  display: grid;
+  place-content: center;
+
+  div {
+    width: 90vw;
+    padding: 1rem 3rem;
+
+    text-align: center;
+    h1 {
+      font-family: Arial, Helvetica, sans-serif;
+      color: cornflowerblue;
+      span {
+        box-shadow: 1px 1px 1px red;
+      }
+    }
+  }
+}
